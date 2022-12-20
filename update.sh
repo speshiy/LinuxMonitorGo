17b936aad395d1cccb80e1d3433aedf91b9f6ab8
speshiy <pssolo@bk.ru>
2022-12-21 00:37:17 +0600

    c

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
-#Останавливаем службу
 systemctl stop linux-monitor-go-service
-#Удаляем бинарник
 rm -rf /root/linux-monitor-go-service/LinuxMonitorGo_linux_amd64
 cp LinuxMonitorGo_linux_amd64 /root/linux-monitor-go-service/LinuxMonitorGo_linux_amd64
-#Стартуем службу
+chmod 776 /root/linux-monitor-go-service/LinuxMonitorGo_linux_amd64
 systemctl start linux-monitor-go-service
