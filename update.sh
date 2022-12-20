57c9487a3051d501347f22feb961402f7879bd46
speshiy <pssolo@bk.ru>
2022-12-21 00:22:09 +0600

    c

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #Останавливаем службу
 systemctl stop linux-monitor-go-service
 #Удаляем бинарник
-rm -rf /root/linux-monitor-go-service/tuvis-server
-cp tuvis-server /root/linux-monitor-go-service/LinuxMonitorGo_linux_amd64
+rm -rf /root/linux-monitor-go-service/LinuxMonitorGo_linux_amd64
+cp LinuxMonitorGo_linux_amd64 /root/linux-monitor-go-service/LinuxMonitorGo_linux_amd64
 #Стартуем службу
 systemctl start linux-monitor-go-service
